89f6885eeeba66624e0db5963ca3935163bf51df
zidnafaz <zidnafaza27@gmail.com>
2023-10-10 16:26:02 +0700

    Contoh pada slide PPT

--- For18.java
+++ For18.java
@@ -2,7 +2,7 @@ public class For18 {
     public static void main(String[] args) {
         
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Hasil kuadrat dari " + i + " adalah " + (i *i));
+            System.out.println("Hasil kuadrat dari " + i + " adalah " + (i * i));
         }
 
     }    
